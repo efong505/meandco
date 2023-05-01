cadaf9de0c33bb6a4f315db8554495be3a38d856
EDWARD FONG <efong@cnm.edu>
2023-05-01 17:18:17 -0600

    Move base templates outside of project

--- resume/views.py
+++ resume/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render, redirect
-
+from .models import Header
 # Create your views here.
+
+def about(request):
+    about = Header.objects.all()
+    context = {'about': about}
+    return render(requeset, 'resume/about.html', context)
