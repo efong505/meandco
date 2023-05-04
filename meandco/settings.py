34fddb9b018a7c81d19c4f351be6fa381954ca78
EDWARD FONG <efong@cnm.edu>
2023-05-04 08:55:39 -0600

    update

--- meandco/settings.py
+++ meandco/settings.py
@@ -139,8 +139,9 @@ LOGOUT_URL = 'logout'
 
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = #<hidden>
-EMAIL_HOST_PASSWORD = #<hidden>
+EMAIL_HOST_USER = EMAIL_ADDRESS
+
+EMAIL_HOST_PASSWORD = SECRET_PASSWORD
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
